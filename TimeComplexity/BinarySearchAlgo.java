f06edfbdb4cc289c21d108dafcba3ec717d0611e
abdullah58091 <abdullahjd6@gmail.com>
2026-03-05 22:05:14 +0530

    update code

--- TimeComplexity/BinarySearchAlgo.java
+++ TimeComplexity/BinarySearchAlgo.java
@@ -25,5 +25,44 @@ public class BinarySearchAlgo {
             System.out.println("name " + (i+1) + "is :" + name[i]);
         }
 
+        //  problem 3 Find the maximum & minimum number in an array of integers.
+
+        int numbers[] = new int[size];
+        for(int i=0; i<size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<numbers.length; i++) {
+            if(numbers[i] < min) {
+                min = numbers[i];
+            }
+            if(numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        System.out.println("Largest number is : " + max);
+        System.out.println("Smallest number is : " + min);
+
+
+        // Take an array of numbers as input and check if it is an array sorted in ascending order.
+
+        int numbers1[] = new int[size];
+        for(int i=0; i<size; i++) {
+            numbers1[i] = sc.nextInt();
+        }
+        boolean isAscending = true;
+        for(int i=0; i<numbers1.length-1; i++) { // NOTICE numbers.length - 1 as termination condition
+            if(numbers1[i] > numbers1[i+1]) { // This is the condition for descending order
+                isAscending = false;
+            }
+        }
+        if(isAscending) {
+            System.out.println("The array is sorted in ascending order");
+        } else {
+            System.out.println("The array is not sorted in ascending order");
+        }
+
+
     }
 }
