c7b2f8641304933a8aef7f20de847ea4a6fa548d
abdullah58091 <abdullahjd6@gmail.com>
2026-03-17 22:24:23 +0530

    Bactracking advance question in java dsa

--- Backtracking_dsa/Tree_visualization.java
+++ Backtracking_dsa/Tree_visualization.java
@@ -1,5 +1,8 @@
 package com.company.Backtracking_dsa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tree_visualization {
 
 
@@ -20,9 +23,43 @@ public class Tree_visualization {
         }
     }
 
+    public static void   findSubset (int  i, int [] nums, List<Integer> current1, List<List<Integer>> result ) {
+
+        // base case
+
+        if( i == nums.length) {
+            result.add(new ArrayList<>(current1));
+            return;
+        }
+
+            // Include elements
+             current1.add(nums[i]);
+            findSubset(i + 1, nums, current1, result);
+
+
+            // backtrack
+            current1.remove(current1.size() - 1);
+
+            // exclude elements
+
+            findSubset( i +1,nums,current1,result);
+
+    }
+
     public static void main(String[] args) {
         System.out.println("tree visualization step recursion");
-        String str ="ABC";
+        String str = "ABC";
         printPermutation(str, "", 0);  // Time complexity - O(n*n!)
+
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> result = new ArrayList<>();
+
+        findSubset(0, nums, new ArrayList<>(), result);
+
+        System.out.println("Backtracking for array");
+
+        System.out.println(result); //⭐ Time Complexity  👉 O(2ⁿ)  Because har element ke 2 choices  ⭐ Space Complexity👉 O(n) recursion stack
+
+
     }
 }
