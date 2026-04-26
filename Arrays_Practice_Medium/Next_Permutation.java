3193611749ffa3983331197c60b098fbeae0b8b6
abdullah58091 <abdullahjd6@gmail.com>
2026-04-26 19:45:57 +0530

    Day 12: permutaion  problem solve

--- Arrays_Practice_Medium/Next_Permutation.java
+++ Arrays_Practice_Medium/Next_Permutation.java
@@ -1,8 +1,41 @@
 public class Next_Permutation {
 
     public static void main(String[] args) {
-        
+        int [] nums = {1,99};
 
-    System.out.println("Hello world!");
+        int n =  nums.length;
+        int i = n-2;
+
+        while(i >= 0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+
+        if(i >= 0){
+            int  j = n-1;
+            while(nums[j] <= nums[i]){
+                j--;
+            }
+            swap(nums,i ,j);
+        }
+
+        reverse(nums, i+1,n-1);
+
+        for(int num : nums){
+            System.out.print(num + " ");
+        }
+    }
+
+    private static void swap(int[] nums ,int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp; 
+    }
+
+    private static  void reverse(int [] nums, int start,int end){
+        while(start < end){
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
     }
 }
