01fb9c72624c4faad3791b24ab855f914f09ced9
abdullah58091 <abdullahjd6@gmail.com>
2026-04-30 22:08:59 +0530

    day 16: Maximum element find

---
+++ Arrays_Practice_Medium/Find_Max_Array.java
@@ -0,0 +1,20 @@
+public class Find_Max_Array {
+    public int findMax(int arr[]){
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+
+        Find_Max_Array call = new Find_Max_Array();
+
+        System.out.println(call.findMax(arr));
+    }
+}
