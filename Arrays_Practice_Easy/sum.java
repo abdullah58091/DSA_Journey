08f8837a3d18b57d1032f1bec47f61ab9c71bf34
abdullah58091 <abdullahjd6@gmail.com>
2026-05-14 10:47:36 +0530

    day 19: problem  is check if array is good

---
+++ Arrays_Practice_Easy/sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main(String[] args) {
+        int [] nums = {1,2,3,4,5};
+        int sum  = 0 ;
+        for(int num : nums){
+            sum += num ;
+        }
+        System.out.println(sum);
+    }
+}
