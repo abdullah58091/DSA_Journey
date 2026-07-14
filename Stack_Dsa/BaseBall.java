8e9f9200971a333fa3b05fa83b0f5eca11d8a726
abdullah58091 <abdullahjd6@gmail.com>
2026-07-14 11:30:33 +0530

    problem solve baseball game

---
+++ Stack_Dsa/BaseBall.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+public class BaseBall {
+
+public static int calPoints(String[] operations) 
+    {
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < operations.length; i++)
+        {
+            if(operations[i].equals("+"))
+            {
+                int top1 = stack.peek(); 
+                stack.pop();
+                int top2 = stack.peek();
+                stack.push(top1);
+                stack.push(top1 + top2);
+            }
+            else if(operations[i].equals("D"))
+            {
+                stack.push(stack.peek() * 2);
+            }
+            else if(operations[i].equals("C"))
+            {
+                stack.pop();
+            }
+            else
+            {
+                stack.push(Integer.parseInt(operations[i]));
+            }
+        }
+        int sum = 0;
+        while(!stack.isEmpty())
+        {
+            sum += stack.peek();
+            stack.pop();
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+    int result = calPoints(new String[]{"5","2","C","D","+"});
+    System.out.println(result);
+}
+}
